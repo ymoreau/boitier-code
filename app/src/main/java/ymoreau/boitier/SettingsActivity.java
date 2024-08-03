d8dd1123a031cdf9e113457e72e2232fba7206ea
Yoann Moreau <ymoreau@MacBook-Pro-de-Yoann.local>
2024-08-03 22:35:14 +0200

    Fix lint Java

--- app/src/main/java/ymoreau/boitier/SettingsActivity.java
+++ app/src/main/java/ymoreau/boitier/SettingsActivity.java
@@ -2,8 +2,6 @@ package ymoreau.boitier;
 
 import android.os.Bundle;
 import android.preference.ListPreference;
-import android.preference.Preference;
-import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceActivity;
 
 import ymoreau.boitier.data.DataModel;
@@ -33,20 +31,18 @@ public class SettingsActivity extends PreferenceActivity {
         listQuestionCount.setSummary(listQuestionCount.getValue());
 
         // Set the listener to update the summary
-        listQuestionCount.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
-            public boolean onPreferenceChange(Preference preference, Object newValue) {
-                preference.setSummary(newValue.toString());
+        listQuestionCount.setOnPreferenceChangeListener((preference, newValue) -> {
+            preference.setSummary(newValue.toString());
 
-                try {
-                    int newCount = Integer.parseInt(newValue.toString());
-                    if (DataModel.ref().answersCount() != newCount)
-                        DataModel.ref().reset();
-                    return true;
-                } catch (NumberFormatException ignored) {
-                }
-
-                return false;
+            try {
+                int newCount = Integer.parseInt(newValue.toString());
+                if (DataModel.ref().answersCount() != newCount)
+                    DataModel.ref().reset();
+                return true;
+            } catch (NumberFormatException ignored) {
             }
+
+            return false;
         });
     }
 }
